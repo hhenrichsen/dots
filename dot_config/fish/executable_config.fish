efd74a56ad4625b815337c3ee6ac09ce5c9c9cc4
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2026-08-17 18:09:27 -0600

    :bug: Fix eza issue, track catppuccin theme

--- dot_config/fish/executable_config.fish
+++ dot_config/fish/executable_config.fish
@@ -30,7 +30,6 @@ if status is-interactive # Commands to run in interactive sessions can go here
     alias vim="nvim"
     alias cat="batcat"
     alias ls="eza"
-    alias ls 'eza --icons'
     alias clear "printf '\033[2J\033[3J\033[1;1H'"
     alias q 'qs -c ii'
 
