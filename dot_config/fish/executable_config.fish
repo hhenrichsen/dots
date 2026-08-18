10c44eb1378bcd004a9af1c44e41bad4f431f737
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2026-08-17 18:01:37 -0600

    :sparkles: Automatically manage certain usual tools I want

--- dot_config/fish/executable_config.fish
+++ dot_config/fish/executable_config.fish
@@ -1,10 +1,22 @@
 fish_add_path "$HOME/.local/bin" "$HOME/.fly/bin" "$HOME/bin"
 
+if command -q brew
+    brew shellenv fish | source
+else if test -x /opt/homebrew/bin/brew
+    /opt/homebrew/bin/brew shellenv fish | source
+else if test -x /home/linuxbrew/.linuxbrew/bin/brew
+    /home/linuxbrew/.linuxbrew/bin/brew shellenv fish | source
+end
+
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
 zoxide init fish | source
+
+if test -x "$HOME/.local/bin/mise"
+    "$HOME/.local/bin/mise" activate fish | source
+end
 
 if status is-interactive # Commands to run in interactive sessions can go here
     # No greeting
